5fc75b505ea9f2efac59ccff80656076605fe91b
FabianCN22 <facamposn@gmail.com>
2026-06-02 08:09:40 +0000

    Jun 2, 2026, 2:09 AM

--- ejercicio1.c
+++ ejercicio1.c
@@ -4,8 +4,4 @@ int main() {
     printf("Hola mundo\n");
     return 0;
 }
-// mensaje de prueba//
-//porfavorrrrrrrrrrrrrrrrrrrrrrrrrr, segunda prueba//
-//otra modificacion 12:24//
-// segunda prueba 12:27
-//nueva prueba
+//ya quedo
