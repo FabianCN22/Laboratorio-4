2f61a2718e8a562388112349b50b74aa3b3c82e2
FabianCN22 <facamposn@gmail.com>
2026-06-03 09:06:09 +0000

    Jun 3, 2026, 3:06 AM

--- ejercicio1.c
+++ ejercicio1.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+//funcion que recorre la matriz para encontyr secuencias de 1
+void findLargestLine(int **matrix, int size, int *result) {
+    int total = size * size;      //total de elementos
+    int currentLine = 0;          //Cantidad actual de 1 consecutivos
+    int largestLine = 0;          //Mayor cantidad de 1 consecutivos
+
+    int *p = *(matrix + 0);
+    for (int i = 0; i < total; i++) {
+        if (*(p + i) == 1) {
+            currentLine++;
+            if (currentLine > largestLine) {
+                largestLine = currentLine;
+            }
+        } else {
+            currentLine = 0;
+        }
+    }
+    *result = largestLine;
+}
