e9e9603b284727dd37bd03b7d883c0ec8c7a2390
FabianCN22 <facamposn@gmail.com>
2026-06-03 09:35:07 +0000

    Jun 3, 2026, 3:35 AM

--- ejercicio1.c
+++ ejercicio1.c
@@ -60,3 +60,10 @@ void printMatrix(int **matrix, int size) {
         printf("\n");
     }
 }
+
+void freeMatrix(int **matrix, int size) {
+    for (int i = 0; i < size; i++) {
+        free(*(matrix + i)); 
+    }
+    free(matrix);
+}        
