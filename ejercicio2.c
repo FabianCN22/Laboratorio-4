52933a354056a86256b69b5fa3b608e34b5c3560
FabianCN22 <facamposn@gmail.com>
2026-06-04 03:03:35 -0600

    Ejercicio 3 completado

---
+++ ejercicio2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned char *read_pgm(const char *archivo, int *width, int *height, int *max_val) {
+    FILE *file = NULL;
+    unsigned char *pixels = NULL;
+    char p, two;
+    int total;
+    int value;
+    file = fopen(archivo, "r"); //apertura de archivo
+    if (file == NULL) {
+        printf("no se pudo abrir el archivo %s\n", archivo);
+        return NULL;
+    }
+    if (fscanf(file, " %c%c", &p, &two) != 2) {
+        printf("no se pudo leer el formato del archivo.\n");
+        fclose(file);
+        return NULL;
+    }
+    if (p != 'P' || two != '2') {
+        printf("el archivo no esta en formato P2.\n");
+        fclose(file);
+        return NULL;
+    }
+    //leer alto y ancho
+    if (fscanf(file, "%d %d", width, height) != 2) {
+        printf("no se pudo leer el ancho y el alto.\n");
+        fclose(file);
+        return NULL;
+    }
+
+    //leer max de pixeles
+    if (fscanf(file, "%d", max_val) != 1) {
+        printf("no se pudo leer el valor maximo.\n");
+        fclose(file);
+        return NULL;
+    }
+
+    //validacion de valores del archivo
+    if (*width <= 0 || *height <= 0 || *max_val <= 0 || *max_val > 255) {
+        printf("Error: datos invalidos en el encabezado.\n");
+        fclose(file);
+        return NULL;
+    }
+    total = (*width) * (*height);
+
+    pixels = malloc((size_t)total * sizeof(unsigned char));
+    if (pixels == NULL) {
+        printf("no se pudo reservar memoria para los pixeles.\n");
+        fclose(file);
+        return NULL;
+    }
